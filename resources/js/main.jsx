87003dc5449865ab6caeadb376588c29650e388a
Mohammad Fayaz <mfayaz7175@gmail.com>
2026-07-25 08:47:38 +0430

    aa

--- resources/js/main.jsx
+++ resources/js/main.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { registerSW } from 'virtual:pwa-register';
+
 import App from './App';
 import './i18n';
-
-// Import CSS - try different approaches
-import '../css/app.css'; // Laravel default location
-// OR
-// import './assets/tailwind.css';
-// import './style.css';
-
+import '../css/app.css';
 import './autoLogout';
+
+registerSW({
+    immediate: true,
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
